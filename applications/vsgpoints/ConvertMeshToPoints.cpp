0e84968fb5698d6fcf0e50fe8ea9c67acd3938f5
Robert Osfield <robert@openscenegraph.com>
2023-05-31 17:03:16 +0100

    Added ConvertMeshToPoints class to populate vsg::Bricks with points from a scene graph. Added --flat command line option to test building flat scene graph of bricks rather than a PagedLOD hierachy.

---
+++ applications/vsgpoints/ConvertMeshToPoints.cpp
@@ -0,0 +1,190 @@
+#include <vsg/all.h>
+
+#include <vsgPoints/Brick.h>
+
+#include "ConvertMeshToPoints.h"
+
+#include <iostream>
+
+ConvertMeshToPoints::ConvertMeshToPoints(vsg::ref_ptr<vsgPoints::Settings> settings) :
+    bricks(vsgPoints::Bricks::create(settings))
+{
+    arrayStateStack.reserve(4);
+    arrayStateStack.emplace_back(vsg::ArrayState::create());
+}
+
+
+void ConvertMeshToPoints::applyDraw(uint32_t firstVertex, uint32_t vertexCount, uint32_t firstInstance, uint32_t instanceCount)
+{
+    auto& arrayState = *arrayStateStack.back();
+    uint32_t lastIndex = instanceCount > 1 ? (firstInstance + instanceCount) : firstInstance + 1;
+    uint32_t endVertex = firstVertex + vertexCount;
+    auto matrix = localToWorldStack().back();
+
+    vsg::ubvec4 color(255,255,255,255);
+
+    for (uint32_t instanceIndex = firstInstance; instanceIndex < lastIndex; ++instanceIndex)
+    {
+        if (auto vertices = arrayState.vertexArray(instanceIndex))
+        {
+            for (uint32_t i = firstVertex; i < endVertex; ++i)
+            {
+                bricks->add(matrix * vsg::dvec3(vertices->at(i)), color);
+            }
+        }
+    }
+}
+
+void ConvertMeshToPoints::applyDrawIndexed(uint32_t firstIndex, uint32_t indexCount, uint32_t firstInstance, uint32_t instanceCount)
+{
+    auto& arrayState = *arrayStateStack.back();
+    uint32_t lastIndex = instanceCount > 1 ? (firstInstance + instanceCount) : firstInstance + 1;
+    uint32_t endIndex = firstIndex + indexCount;
+    auto matrix = localToWorldStack().back();
+
+    vsg::ubvec4 color(255,255,255,255);
+
+    if (ushort_indices)
+    {
+        for (uint32_t instanceIndex = firstInstance; instanceIndex < lastIndex; ++instanceIndex)
+        {
+            if (auto vertices = arrayState.vertexArray(instanceIndex))
+            {
+                for (uint32_t i = firstIndex; i < endIndex; ++i)
+                {
+                    bricks->add(matrix * vsg::dvec3(vertices->at(ushort_indices->at(i))), color);
+                }
+            }
+        }
+    }
+    else if (uint_indices)
+    {
+        for (uint32_t instanceIndex = firstInstance; instanceIndex < lastIndex; ++instanceIndex)
+        {
+            if (auto vertices = arrayState.vertexArray(instanceIndex))
+            {
+                for (uint32_t i = firstIndex; i < endIndex; ++i)
+                {
+                    bricks->add(matrix * vsg::dvec3(vertices->at(uint_indices->at(i))), color);
+                }
+            }
+        }
+    }
+}
+
+void ConvertMeshToPoints::apply(const vsg::Node& node)
+{
+    std::cout<<node.className()<<std::endl;
+    node.traverse(*this);
+}
+
+void ConvertMeshToPoints::apply(const vsg::StateGroup& stategroup)
+{
+    auto arrayState = stategroup.prototypeArrayState ? stategroup.prototypeArrayState->clone(arrayStateStack.back()) : arrayStateStack.back()->clone();
+
+    for (auto& statecommand : stategroup.stateCommands)
+    {
+        statecommand->accept(*arrayState);
+    }
+
+    arrayStateStack.emplace_back(arrayState);
+
+    stategroup.traverse(*this);
+
+    arrayStateStack.pop_back();
+}
+
+void ConvertMeshToPoints::apply(const vsg::Transform& transform)
+{
+    auto& l2wStack = localToWorldStack();
+
+    vsg::dmat4 localToWorld = l2wStack.empty() ? transform.transform({}) : transform.transform(l2wStack.back());
+    l2wStack.push_back(localToWorld);
+
+    transform.traverse(*this);
+
+    l2wStack.pop_back();
+}
+
+void ConvertMeshToPoints::apply(const vsg::LOD& lod)
+{
+    std::cout<<lod.className()<<" "<<std::endl;
+    lod.traverse(*this);
+}
+
+void ConvertMeshToPoints::apply(const vsg::PagedLOD& plod)
+{
+    std::cout<<plod.className()<<" "<<std::endl;
+    plod.traverse(*this);
+}
+
+void ConvertMeshToPoints::apply(const vsg::VertexDraw& vid)
+{
+    auto& arrayState = *arrayStateStack.back();
+    arrayState.apply(vid);
+    if (!arrayState.vertices) return;
+
+    applyDraw(vid.firstVertex, vid.vertexCount, vid.firstInstance, vid.instanceCount);
+}
+
+void ConvertMeshToPoints::apply(const vsg::VertexIndexDraw& vid)
+{
+    auto& arrayState = *arrayStateStack.back();
+    arrayState.apply(vid);
+    if (!arrayState.vertices) return;
+
+    if (vid.indices) vid.indices->accept(*this);
+
+    applyDrawIndexed(vid.firstIndex, vid.indexCount, vid.firstInstance, vid.instanceCount);
+}
+
+void ConvertMeshToPoints::apply(const vsg::Geometry& geometry)
+{
+    auto& arrayState = *arrayStateStack.back();
+    arrayState.apply(geometry);
+    if (!arrayState.vertices) return;
+
+    if (geometry.indices) geometry.indices->accept(*this);
+
+    for (auto& command : geometry.commands)
+    {
+        command->accept(*this);
+    }
+}
+
+void ConvertMeshToPoints::apply(const vsg::BindVertexBuffers& bvb)
+{
+    arrayStateStack.back()->apply(bvb);
+}
+
+void ConvertMeshToPoints::apply(const vsg::BindIndexBuffer& bib)
+{
+    bib.indices->accept(*this);
+}
+
+void ConvertMeshToPoints::apply(const vsg::BufferInfo& bufferInfo)
+{
+    if (bufferInfo.data) bufferInfo.data->accept(*this);
+}
+
+void ConvertMeshToPoints::apply(const vsg::ushortArray& array)
+{
+    ushort_indices = &array;
+    uint_indices = nullptr;
+}
+
+void ConvertMeshToPoints::apply(const vsg::uintArray& array)
+{
+    ushort_indices = nullptr;
+    uint_indices = &array;
+}
+
+void ConvertMeshToPoints::apply(const vsg::Draw& draw)
+{
+    applyDraw(draw.firstVertex, draw.vertexCount, draw.firstInstance, draw.instanceCount);
+}
+
+void ConvertMeshToPoints::apply(const vsg::DrawIndexed& drawIndexed)
+{
+    applyDrawIndexed(drawIndexed.firstIndex, drawIndexed.indexCount, drawIndexed.firstInstance, drawIndexed.instanceCount);
+}
---
+++ applications/vsgpoints/ConvertMeshToPoints.h
@@ -0,0 +1,41 @@
+#include <vsg/all.h>
+
+#include <vsgPoints/Brick.h>
+
+#include <iostream>
+
+class ConvertMeshToPoints : public vsg::Inherit<vsg::ConstVisitor, ConvertMeshToPoints>
+{
+public:
+
+    ConvertMeshToPoints(vsg::ref_ptr<vsgPoints::Settings> settings);
+
+    vsg::ref_ptr<vsgPoints::Bricks> bricks = vsgPoints::Bricks::create();
+
+    using ArrayStateStack = std::vector<vsg::ref_ptr<vsg::ArrayState>>;
+    ArrayStateStack arrayStateStack;
+    vsg::ref_ptr<const vsg::ushortArray> ushort_indices;
+    vsg::ref_ptr<const vsg::uintArray> uint_indices;
+
+    /// get the current local to world matrix stack
+    std::vector<vsg::dmat4>& localToWorldStack() { return arrayStateStack.back()->localToWorldStack; }
+
+    void applyDraw(uint32_t firstVertex, uint32_t vertexCount, uint32_t firstInstance, uint32_t instanceCount);
+    void applyDrawIndexed(uint32_t firstIndex, uint32_t indexCount, uint32_t firstInstance, uint32_t instanceCount);
+
+    void apply(const vsg::Node& node) override;
+    void apply(const vsg::StateGroup& stategroup) override;
+    void apply(const vsg::Transform& transform) override;
+    void apply(const vsg::LOD& lod) override;
+    void apply(const vsg::PagedLOD& plod) override;
+    void apply(const vsg::VertexDraw& vid) override;
+    void apply(const vsg::VertexIndexDraw& vid) override;
+    void apply(const vsg::Geometry& geometry) override;
+    void apply(const vsg::BindVertexBuffers& bvb) override;
+    void apply(const vsg::BindIndexBuffer& bib) override;
+    void apply(const vsg::BufferInfo& bufferInfo) override;
+    void apply(const vsg::ushortArray& array) override;
+    void apply(const vsg::uintArray& array) override;
+    void apply(const vsg::Draw& draw) override;
+    void apply(const vsg::DrawIndexed& drawIndexed) override;
+};
